1b885bd21d046d9b6a33c0eed39b0e30463226d2
Erik Silva <contato@erikhenrique.com.br>
2022-08-15 14:21:33 -0300

    List rows to extract on csv exporter

--- exporters/csv_exporter.py
+++ exporters/csv_exporter.py
@@ -5,13 +5,48 @@ from exporters.exporter import Exporter
 
 class CSVExport(Exporter):
 
+    rows = [
+        'key',
+        'summary',
+        'issuetype',
+        'labels',
+        'assignee',
+        'created',
+        'resolutiondate',
+        'customfield_10024',
+        'status',
+        'In Progress',
+        'A Iniciar',
+        'Testes',
+        'Concluído',
+        'Validação 2',
+        'Pool de Ideias',
+        'Aguardando Desenvolvimento',
+        'Em Homologação',
+        'Atividade bloqueada',
+        'Backlog',
+        'Pronto p/ DEV',
+        'Detalhamento da Hipótese',
+        'Code Review',
+        'Cancelado',
+        'Priorizados',
+        'Discovery Concluído',
+        'Em desenvolvimento',
+        'Atividade finalizada',
+        'Refining',
+        'Done',
+        'upstream',
+        'downstream'
+    ]
+
+
     def export(self, issues, metric) -> None:
         issues_merged = self.merge_metric(issues, metric)
 
-        headers = self.build_headers(issues_merged)
+        # headers = self.build_headers(issues_merged)
 
         with open('csv_exporter.csv', 'w', newline='') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=headers)
+            writer = csv.DictWriter(csvfile, fieldnames=self.rows)
             writer.writeheader()
 
             for issue in issues_merged:
