98eae58d8fae241795706f50b285c04e92754b0f
Erik Silva <contato@erikhenrique.com.br>
2022-08-15 14:23:16 -0300

    Add upstream and downstream calc

--- metrics/status.py
+++ metrics/status.py
@@ -3,16 +3,16 @@ from dateutil import parser
 from collections import defaultdict
 
 
-def time_by_status(issues) -> None:
 
+def time_by_status(issues, upstream_statuses, downstream_statuses) -> None:
     metric = {
         'all_statuses': set(),
         'results': []
     }
 
     for issue in issues:
-        total_seconds_status = defaultdict(float)
-        total_seconds_status['key'] = issue.key
+        total_hours_status = defaultdict(float)
+        total_hours_status['key'] = issue.key
         last_event_date = None
 
         for history in issue.changelog.histories:
@@ -24,11 +24,19 @@ def time_by_status(issues) -> None:
                     metric['all_statuses'].add(item.fromString)
 
                     if last_event_date:
-                        total_time_in_hours = int((last_event_date - created).total_seconds() / 60)
-                        total_seconds_status[item.toString] += total_time_in_hours
+                        total_time_in_hours = int(
+                            (last_event_date - created).total_seconds() / 60
+                        )
+                        total_hours_status[item.toString] += total_time_in_hours
+
+                        if item.toString in upstream_statuses:
+                            total_hours_status['upstream'] += total_time_in_hours
+
+                        if item.toString in downstream_statuses:
+                            total_hours_status['downstream'] += total_time_in_hours
                     
                     last_event_date = created
 
-        metric['results'].append(total_seconds_status)
+        metric['results'].append(total_hours_status)
 
     return metric
